d88b5ff0e6e4395d4870557568dea61dd458f4ef
ntz448 <77314033+natizen448@users.noreply.github.com>
2021-05-30 23:21:31 +0900

    namespace 개념설명

---
+++ Study/02Namespace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//문법을 공부할때,
+//응용법까지 욕심내려는 경우가있다.
+//게임으로 프로그래밍에서는
+//실제 코드를 짜면서 보기전까지는
+//응용에 대해서 느낄수가 없다.
+//프로그래밍에서는 경우가 무한대이다.
+
+//네임스페이스는 개념의 표현이라기보단
+//개념의 분류
+
+//https://www.youtube.com/watch?v=EhR0g5gBGaA&list=PL4SIC1d_ab-Y-bBKojxhtFWwNpawMM1h5&index=4
+
