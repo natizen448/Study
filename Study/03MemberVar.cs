da507d7d7750119e95faf1f9e307a0893be1631a
ntz448 <77314033+natizen448@users.noreply.github.com>
2021-06-02 18:22:59 +0900

    맴버변수와 상수 기본 개념

---
+++ Study/03MemberVar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//한글은 보통 사용하지않는다.
+//쓸수없다는것은 아니지만
+//일단 모든 이런 시스템이 영어의 코드로 쳐졌다.
+//한글의 처리가 미흡한 점들이 몇몇 곳에서 있다.
+
+
+class Player
+{
+    //멤버변수의 선언은
+    //int(자료형) 이름(식별자)
+    //초기값을 넣어줄수있다.
+    //리터럴값이라고 한다.
+    //선언을 함과 동시에 값을 넣어주는것을
+    //리터럴 초기화라고 한다.
+
+
+    //검색능력도 프로그래머의 능력
+    //남의 지식을 얼마나 잘쓰느냐?
+
+    //자료형의 의미
+    //크기
+    //형태 : 정수
+    //이름의 의미
+    //위치
+    //내가 사용하기위한 별명
+
+    //https://www.youtube.com/watch?v=dTc_Ty9D_wo&list=PL4SIC1d_ab-Y-bBKojxhtFWwNpawMM1h5&index=6
+}
